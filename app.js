60f1d274ccf2a34fb6fc9adcab9022dc018c187f
gurb <Organicmintz@gmail.com>
2022-06-16 20:45:56 -0700

    import route handler

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
-const sequelize = require('./models').sequelize;
+const { sequelize } = require('./models');
+const routes = require('./routes');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -29,6 +30,8 @@ app.get('/', (req, res) => {
     message: 'Welcome to the REST API project!',
   });
 });
+
+app.use('/api', routes);
 
 // send 404 if no other route matched
 app.use((req, res) => {
