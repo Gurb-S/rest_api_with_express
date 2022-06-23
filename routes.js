1261848b9bcd55a5e24bb3f2cc185751fdc540a8
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-06-23 12:29:51 -0700

    display owner on courses

--- routes.js
+++ routes.js
@@ -21,9 +21,9 @@ router.get('/users', authenticateUser, asyncHandler(async (req,res) => {
 router.post('/users', asyncHandler(async (req,res) => {
     try{
         await User.create(req.body);
-        res.status(201).json({ "message": "Account successfully created!"})
+        res.status(201).end();
     } catch(error){
-        if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueComstraintError'){
+        if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError'){
             const errors = error.errors.map(err => err.message);
             res.status(400).json({ errors })
         }
@@ -36,21 +36,53 @@ router.post('/users', asyncHandler(async (req,res) => {
 router.get('/courses', asyncHandler(async (req,res) => {
     try{
         const course = await Course.findAll();
+        const usersData = await User.findAll({ raw: true});
+        //const usersDataJSON = usersData.toJSON();
+        //const user = req.currentUser.toJSON();
+        //console.log(usersData)
+        const users = usersData.map(user => ({
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            emailAddress: user.emailAddress
+        }))
+        console.log(users)
         console.log('SUCESSSSS')
         const courses = course.map(course => 
-        [  
-            course.title, 
-            course.description, 
-            course.estimatedTime, 
-            course.materialsNeeded
-        ])
+        ({  
+            title: course.title,
+            description: course.description,
+            estimatedTime: course.estimatedTime,
+            materialsNeeded: course.materialsNeeded,
+            userId: course.userId,
+            owner: {
+
+            }
+            // if(course.userId === users.id){
+            //     return { firstName: users.firstName, lastName: users.lastName }
+            // }
+        }))
         console.log(courses)
+        // courses.users.push("test user")
+        for(let i = 0; i < courses.length; i++){
+            //console.log(`${users[0].id}`)
+           // console.log(i)
+           for(let j = 0; j < users.length; j++){
+            if(courses[i].userId === users[j].id){
+                //console.log(`${users[i].id}`)
+                courses[i].owner['name'] = `${users[j].firstName} ${users[j].lastName}`,
+                courses[i].owner['emailAddress'] = users[j].emailAddress
+            }
+           }
+        }
+        //courses[0].users['Owner 1'] = users[0].firstName;
+        //console.log(courses[3].description)
         res.json({
             courses
         })
     } catch(error){
         console.log('ERORRRRRRRRR')
-        if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueComstraintError'){
+        if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError'){
             const errors = error.errors.map(err => err.message);
             res.status(400).json({ errors })
         }
@@ -104,24 +136,30 @@ router.put('/courses/:id', authenticateUser, asyncHandler(async (req,res) => {
     const { id } = req.params;
     const { userId } = req.body;
     const user = req.currentUser.toJSON();
+    console.log(user)
     try{
-        if(user.id === userId){
+        const course = await Course.findOne({
+            where: {
+                id: id,
+                userId: user.id
+            }
+        })
+        if(course){
             await Course.update(req.body, {
                 where: {
-                    id: id,
-                    userId: userId
+                    id: id
                 }
             });
             console.log(req.body)
             console.log('UPDATEDDDDDDDDD')
-            res.status(204)
+            res.status(204).end();
         }
         else{
             console.log('DENIEDDDDDDD')
             res.status(403).json({ "message": "You are not the owner of this course!"})
         }
 
-    } catch(error){
+    } catch(error){ 
         if(error.name === 'SequelizeValidationError'){
             const errors = error.errors.map(err => err.message);
             res.status(400).json({ errors })
